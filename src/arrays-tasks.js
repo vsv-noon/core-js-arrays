c89f435e48fa52ace0eef71f5b34bc8602e0ae35
ViacheslavAnisimov <forvsn@gmail.com>
2024-01-11 04:49:02 +0700

    solved new task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -38,8 +38,11 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  if (arr1.length > arr2.length) {
+    return arr1.map((el, i) => el + (arr2[i] || 0));
+  }
+  return arr2.map((el, i) => el + (arr1[i] || 0));
 }
 
 /**
