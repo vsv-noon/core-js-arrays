eb938c32faae21d750a030ad7379fea8cce8a18a
ViacheslavAnisimov <forvsn@gmail.com>
2024-01-13 06:51:26 +0700

    feat: solved new tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -123,8 +123,12 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const result = arr.reduce((acc, el) => acc + el);
+  return parseFloat((result / arr.length).toFixed(2));
 }
 
 /**
@@ -137,8 +141,8 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((el) => el.length === arr[0].length);
 }
 
 /**
