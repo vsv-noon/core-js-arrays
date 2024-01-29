6ec0fe51de14eee828e2c7cde662e4920aa5bd5f
ViacheslavAnisimov <forvsn@gmail.com>
2024-01-30 06:09:24 +0700

    feat: solved new task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -359,8 +359,8 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return new Array(len).fill(0).map((el, i) => 2 * i + 1);
 }
 
 /**
